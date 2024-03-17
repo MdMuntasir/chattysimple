c5403cdf0908cab2972fdf26590872c03bdbf73a
Muntasir <md.muntasir2700@gmail.com>
2024-03-17 23:35:43 +0600

    Connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,12 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyCxgsByGKtZjE38dcFfxnQfLSbKokmUe7U",
+            appId: "1:712497068578:android:f2fa3dd0283083c42a687b",
+            messagingSenderId: "712497068578",
+            projectId: "chattysimple-2818b") );
   } catch (e) {
     log('Firebase initialization error: $e');
   }
